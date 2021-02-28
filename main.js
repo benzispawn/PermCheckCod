f8fdf8a0bb4930a3e0d3ef1a05929b39671aa3b1
benzispawn <spawnbenzi@gmail.com>
2021-02-28 11:19:40 -0300

    minimal adjusts

--- main.js
+++ main.js
@@ -4,9 +4,6 @@
  */
 function solution (A) {
     let setA = new Set(A);
-    let result = 1;
-    let proof = 1;
-    let minA = 0;
     let size = A.length;
 
     if (typeof A !== 'object') {
@@ -17,21 +14,11 @@ function solution (A) {
         return 0;
     }
 
-    // if (size == 1 && A[0] !== 1) {
-    //     return 0;
-    // }
-
     for (let i = 0; i < size; i++) {
-        // proof *= i + 1;
-        // result *= A[i];
         if (!setA.has(i+1)) {
             return 0
         }
     }
-
-    // if (result == proof && setA.has(1)) {
-    //     return 1;
-    // }
 
     return 1;
 }
